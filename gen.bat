d03d64c3857d1f44e01a7753fbcadd3649f1a861
altwine <180307749+altwine@users.noreply.github.com>
2026-06-01 23:55:17 +0300

    refactor(`gen.bat`): use pushd/popd instead of cd

--- gen.bat
+++ gen.bat
@@ -1,6 +1,6 @@
 @echo off
 
-:: select currect dir
+:: select currect script dir
 cd /d "%~dp0"
 
 :: clean
@@ -9,12 +9,13 @@ rmdir /s /q ".\windows"
 :: generate .lib for windows
 call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvars64.bat"
 mkdir ".\windows"
-cd ".\quickjs-amalgam"
+pushd ".\quickjs-amalgam"
 cl /nologo /c /std:c11 /experimental:c11atomics /O2 /D_WIN32 /DCONFIG_WIN32 "quickjs-amalgam.c" /Fo..\windows\quickjs.obj
-cd ..\windows
+popd
+pushd .\windows
 lib /nologo /OUT:.\libquickjs.lib .\quickjs.obj
 del /f ".\quickjs.obj"
-cd ".."
+popd
 
 :: put lib dir to odin-quickjs
 move ".\windows" ".\odin-quickjs\windows"
